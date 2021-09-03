832e9c5535af2b6a750371bcb8a4062d1fb6f286
Geoffry Millner <geoff.millner22@gmail.com>
2021-09-03 10:20:12 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Triangle from './triangle.js';
+import Rectangle from './rectangle.js';
+
+$(document).ready(function() {
+  $('#triangle-checker-form').submit(function(event) {
+    event.preventDefault();
+    const length1 = $('#length1').val();
+    const length2 = $('#length2').val();
+    const length3 = $('#length3').val();
+    const triangle = new Triangle(length1, length2, length3)
+    const response = triangle.checkType();
+    $('#response').append("<p>" + response + "</p>");
+  });
+  
+  $('#rectangle-area-form').submit(function(event) {
+    event.preventDefault();
+    const length1 = parseInt($('#rect-length1').val());
+    const length2 = parseInt($('#rect-length2').val());
+    const rectangle = new Rectangle(length1, length2)
+    const response = rectangle.getArea()
+    $('#response2').append(`<p> The are of the rectangle is ${response}.</p>`)
+  })
+});

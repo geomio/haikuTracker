1f059be7380b76b11fad05534ab5541292b2a11f
Geoffry Millner <geoff.millner22@gmail.com>
2021-09-07 16:18:57 -0700

    add haiku object and user input text area, will also split tect into lines

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Haiku from './haiku.js';
+
+$(document).ready(function() {
+  $('#formArea').submit(function(event){
+    event.preventDefault();
+    let userPoemInput = $("#userPoemArea").val();
+    let haikuObject = new Haiku(userPoemInput.split("\n"))
+    console.log(userPoemInput.split("\n"));
+    console.log(haikuObject);
+  });  
+});
+
+//push user input lines into new object
+//count lines in object
+//return wether lines are or are not haiku
+// if lines match count line syllables
+//display user haiku maybe with counts

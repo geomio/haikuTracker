5288e1498d8e687145f3e655e0fc23c3a4521b68
Geoffry Millner <geoff.millner22@gmail.com>
2021-09-10 16:18:03 -0700

    add function class to count and strore syl counts

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -1,29 +1,29 @@
 import Haiku from "../src/haiku";
 
 describe('Haiku', () => {
-  
+
   test('should create a object that holds text in an array', () => {
-    const haiku = new Haiku(['text line one', 'text line 2','text line 3']);
+    const haiku = new Haiku(['text line one', 'text line 2', 'text line 3']);
     expect(haiku.text.length).toEqual(3);
   });
-  
+
   test('linesAndWordSplitter should count the lines of a haiku and see if there are three lines', () => {
-    const haiku = new Haiku(['text line one', 'text line 2','text line 3']);
+    const haiku = new Haiku(['text line one', 'text line 2', 'text line 3']);
     expect(haiku.linesAndWordSplitter()).toEqual(true);
   });
-  
+
   test('linesAndWordSplitter should count the lines of a haiku and see if less three lines are there', () => {
     const haiku = new Haiku(['text line one', 'text line 2',]);
     expect(haiku.linesAndWordSplitter()).toEqual(false);
   });
-  
+
   test('linesAndWordSplitter should count the lines of a haiku and see if more than three lines are present', () => {
-    const haiku = new Haiku(['text line one', 'text line 2','text line 3', 'text line 4']);
+    const haiku = new Haiku(['text line one', 'text line 2', 'text line 3', 'text line 4']);
     expect(haiku.linesAndWordSplitter()).toEqual(false);
   });
-  
+
   test('linesAndWordSplitter should split strings into individual word array into this.line1 ', () => {
-    const haiku = new Haiku(['text line one', 'text line','text']);
+    const haiku = new Haiku(['text line one', 'text line', 'text']);
     haiku.linesAndWordSplitter();
     const line1Length = haiku.line1.length;
     const line2Length = haiku.line2.length;
@@ -32,9 +32,9 @@ describe('Haiku', () => {
     expect(line2Length).toEqual(2);
     expect(line3Length).toEqual(1);
   });
-  
+
   test('maxWordCounter should check to see if lines1 = 5 or less words, line2 = 7 or less words, and line3 = 5 or less words', () => {
-    const haiku = new Haiku(['text line one has five', 'text line must have up to seven','the text is at five']);
+    const haiku = new Haiku(['text line one has five', 'text line must have up to seven', 'the text is at five']);
     haiku.linesAndWordSplitter();
     const line1Length = haiku.line1.length;
     const line2Length = haiku.line2.length;
@@ -43,9 +43,9 @@ describe('Haiku', () => {
     expect(haiku.maxWordCounter(line2Length)).toEqual(true);
     expect(haiku.maxWordCounter(line3Length)).toEqual(true);
   });
-  
+
   test('maxWordCounter should check to see if lines1 = 5 or less words, line2 = 7 or less words, and line3 = 5 or less words', () => {
-    const haiku = new Haiku(['text line one has five and one', 'text line must have up to seven here is too much','the text is at five this also is bad']);
+    const haiku = new Haiku(['text line one has five and one', 'text line must have up to seven here is too much', 'the text is at five this also is bad']);
     haiku.linesAndWordSplitter();
     const line1Length = haiku.line1.length;
     const line2Length = haiku.line2.length;
@@ -54,11 +54,11 @@ describe('Haiku', () => {
     expect(haiku.maxWordCounter(line2Length)).toEqual(false);
     expect(haiku.maxWordCounter(line3Length)).toEqual(false);
   });
-  
+
   test('syllableCounter should count total syllables in a word', () => {
-    const haiku = new Haiku(['text line one has five', 'text line must have up to seven','the text is at five']);
-    expect(haiku.syllableCounter("amy")).toEqual(2);
-    expect(haiku.syllableCounter("closet")).toEqual(2);
+    const haiku = new Haiku(['text line the has five', 'text line must have up to seven', 'the text is at five']);
+    expect(haiku.syllableCounter("any")).toEqual(2);
+    expect(haiku.syllableCounter("louis")).toEqual(2);
     expect(haiku.syllableCounter("continue")).toEqual(3);
     expect(haiku.syllableCounter("continuous")).toEqual(4);
     expect(haiku.syllableCounter("sometime")).toEqual(2);
@@ -66,5 +66,14 @@ describe('Haiku', () => {
     expect(haiku.syllableCounter("pitiful")).toEqual(3);
     expect(haiku.syllableCounter("beautiful")).toEqual(3);
   });
-  
+
+  test('lineSyllableCounter should count total syllables in a line and add it to this.lineSyllableCount1-3', () => {
+    const haiku = new Haiku(['text line the has five', 'text line must have the seven', 'the text is at five']);
+    haiku.linesAndWordSplitter();
+    haiku.lineSyllableCounter();
+    console.table(haiku);
+    expect(haiku.line1SyllableCount).toEqual(5);
+    expect(haiku.line2SyllableCount).toEqual(7);
+    expect(haiku.line3SyllableCount).toEqual(5);
+  });
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,5 +1,5 @@
-export default class Haiku{ 
-  constructor(text,) {
+export default class Haiku {
+  constructor(text) {
     this.text = text
     this.line1 = []
     this.line2 = []
@@ -7,76 +7,90 @@ export default class Haiku{
     this.line1SyllableCount = 0
     this.line2SyllableCount = 0
     this.line3SyllableCount = 0
-}
+  }
 
-linesAndWordSplitter() {
-  if (this.text.length == 3) {
-    console.log('A Haiku has three lines');
-    this.line3 = this.text[2].split(" ");
-    this.line2 = this.text[1].split(" ");
-    this.line1 = this.text[0].split(" ");
-    console.log(this.line1);
-    console.log(this.line2);
-    console.log(this.line3);
-    return true;
-  }if (this.text.length <= 2 ) {
-    console.log('There is not enough lines');
-    return false;
-  }if (this.text.length >=4) {
-    console.log('there are to many lines');
-    return false;
+  linesAndWordSplitter() {
+    if (this.text.length == 3) {
+      console.log('A Haiku has three lines');
+      this.line3 = this.text[2].split(" ");
+      this.line2 = this.text[1].split(" ");
+      this.line1 = this.text[0].split(" ");
+      console.log(this.line1);
+      console.log(this.line2);
+      console.log(this.line3);
+      return true;
+    } if (this.text.length <= 2) {
+      console.log('There is not enough lines');
+      return false;
+    } if (this.text.length >= 4) {
+      console.log('there are to many lines');
+      return false;
+    }
   }
- }
- 
- maxWordCounter() {
-   if (this.line1.length > 5) {
-     console.log("Line 1 has to many syllables - more then 5 are present, use 5 or less");
-     return false;
-   } if (this.line2. length > 7) {
-    console.log("Line 2 has to many syllables - more then 5 are present, use 5 or less");
-    return false;
-   }if (this.line3.length > 5) {
-     console.log("line three has two many syllables 5 are needed");
-     return false;
-   } if (this.line1.length <= 5) {
-     console.log("line1 word count passes second check");
-     return true;
-   }if (this.line2.length <= 7) {
-     console.log('line2 passes first check');
-     return true;
-   }if (this.line3.length <= 5) {
-     console.log('line3 passes first check');
-     return true;
-   }
- }
- 
- syllableCounter(word) {
-   //by brian from stack exchange
-   // on https://stackoverflow.com/questions/5686483/how-to-compute-number-of-syllables-in-a-word-in-javascript
-  word = word.toLowerCase();
-  let t_some = 0;
-  let beauCount = 0;
-  if(word.length>3)
-      {
-      if(word.substring(0,4)=="some")
-          {
-          word = word.replace("some","");
-          t_some++;
-          }else if(word.substring(0,4)=="beau") {
-            word = word.replace("beau","");
-            beauCount++;
-          }
+
+  maxWordCounter() {
+    if (this.line1.length > 5) {
+      console.log("Line 1 has to many syllables - more then 5 are present, use 5 or less");
+      return false;
+    } if (this.line2.length > 7) {
+      console.log("Line 2 has to many syllables - more then 5 are present, use 5 or less");
+      return false;
+    } if (this.line3.length > 5) {
+      console.log("line three has two many syllables 5 are needed");
+      return false;
+    } if (this.line1.length <= 5) {
+      console.log("line1 word count passes second check");
+      return true;
+    } if (this.line2.length <= 7) {
+      console.log('line2 passes first check');
+      return true;
+    } if (this.line3.length <= 5) {
+      console.log('line3 passes first check');
+      return true;
+    }
+  }
+
+  syllableCounter(word) {
+    //by brian from stack exchange
+    // on https://stackoverflow.com/questions/5686483/how-to-compute-number-of-syllables-in-a-word-in-javascript
+    word = word.toLowerCase();
+    let t_some = 0;
+    let beauCount = 0;
+    if (word.length <= 3) { return 1; }
+    if (word.length > 3) {
+      if (word.substring(0, 4) == "some") {
+        word = word.replace("some", "");
+        t_some++;
+      } else if (word.substring(0, 4) == "beau") {
+        word = word.replace("beau", "");
+        beauCount++;
       }
-  word = word.replace(/(?:[^laeiouy]|ed|[^laeiouy]e)$/, '');   
-  word = word.replace(/^y/, '');                                 
-  //return word.match(/[aeiouy]{1,2}/g).length;   
-  let syl = word.match(/[aeiouy]{1,2}/g);
-  console.log(syl);
-  if(syl)
-  {
+    }
+    word = word.replace(/(?:[^laeiouy]|ed|[^laeiouy]e)$/, '');
+    word = word.replace(/^y/, '');
+    //return word.match(/[aeiouy]{1,2}/g).length;   
+    let syl = word.match(/[aeiouy]{1,2}/g);
+    // console.log(syl, "syllables");
+    if (syl) {
       //console.log(syl);
-      return syl.length+t_some + beauCount;
+      return syl.length + t_some + beauCount;
+    }
   }
-}
- 
+
+  lineSyllableCounter() {
+    this.line1.forEach((word) => {
+      this.line1SyllableCount += this.syllableCounter(word);
+    });
+
+    this.line2.forEach((word) => {
+      console.log(word);
+      console.log(this.syllableCounter(word));
+      this.line2SyllableCount += this.syllableCounter(word);
+    });
+
+    this.line3.forEach((word) => {
+      this.line3SyllableCount += this.syllableCounter(word);
+    });
+  }
+
 };

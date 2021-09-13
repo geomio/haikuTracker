d530167063cefe2ac9b0fd6466eca8cccf550f85
Geoffry Millner <geoff.millner22@gmail.com>
2021-09-13 16:09:24 -0700

    added id to span areas

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Haiku from './haiku.js';
 
-$(document).ready(function() {
-  $('#formArea').submit(function(event){
+$(document).ready(function () {
+  $('#formArea').submit(function (event) {
     event.preventDefault();
     let userPoemInput = $("#userPoemArea").val();
     let haikuObject = new Haiku(userPoemInput.split("\n"))
     haikuObject.linesAndWordSplitter();
-  });  
+    haikuObject.syllableCounter();
+    haikuObject.haikuLine1Check();
+    haikuObject.haikuLine2Check();
+    haikuObject.haikuLine3Check();
+    haikuObject.haikuCheck();
+  });
 });
 
 //push user input lines into new object
